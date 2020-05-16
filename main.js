b60f34094098ef32d5a8861a62d0ce56d66e2139
towar4221 <towar4221@gmail.com>
2020-05-16 16:42:31 +0700

    second commit

--- main.js
+++ main.js
@@ -1,25 +1,61 @@
-const Animal = {
-    name: "",
-    move: 0,
-    mov: function(){
-        console.log(this.name + " делает " + this.move)
-    },
-    constrct: function(name, move){
+
+class Animal {
+    constructor(type = "", name = "", color = "", poroda = "", pol  = "", food  = "") {
         this.name = name;
-        this.move = move;
+        this.color = color;
+        this.type = type;
+
+    }
+
+    info() {
+        console.log(this.name + " это " + this.type + " и он " + this.color " цвета " );
     }
 }
 
-const duck = Object.create(Animal)
-duck.constrct("Селезень", "кря")
+class Dog extends Animal {
+    constructor(name, type, color, poroda = "") {
+        super(name, type, color, poroda)
+        this.poroda = poroda;
+    }
 
-const dog = Object.create(Animal)
-dog.constrct("Собака", "гав")
+    getDog() {
+        console.log("Он " + this.poroda);
+    }
+}
 
-const chipmunk = Object.create(Animal)
-chipmunk.constrct("Бурундук", "писк")
+const Tuz = new Dog("тузик", "собака", "чёрного", "мопс")
 
+Tuz.info()
+Tuz.getDog()
 
-duck.mov()
-dog.mov()
-chipmunk.mov()
+class Duck extends Animal {
+    constructor(name, type, color, pol  = "") {
+        super(name, type, color, pol)
+        this.pol = pol;
+    }
+
+    getDuck() {
+        console.log("Он" + this.poroda);
+    }
+}
+
+const Vas = new Duck("Вася", "селезень", "белого", "мужского пола, ведь он селезень")
+
+Vas.info()
+Vas.getDuck()
+
+class Chipmunk extends Animal {
+    constructor(name, type, color, food  = "") {
+        super(name, type, color, food)
+        this.food = food;
+    }
+
+    getChipmunk() {
+        console.log("И он ест " + this.food);
+    }
+}
+
+const Alvin = new Chipmunk("Элвин", "бурундук", "серого", "корм")
+
+Alvin.info()
+Alvin.getChipmunk()
